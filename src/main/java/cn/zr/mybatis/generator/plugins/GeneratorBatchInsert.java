2e93ae926dc2905b523f63283a0c1c3b35bab338
rui.zhang <zhangrui@ejie365.com>
2018-04-09 12:33:33 +0800

    增加自动生成swagger文档注解到实体类

---
+++ src/main/java/cn/zr/mybatis/generator/plugins/GeneratorBatchInsert.java
@@ -0,0 +1,17 @@
+package cn.zr.mybatis.generator.plugins;
+
+import org.mybatis.generator.api.PluginAdapter;
+
+import java.util.List;
+
+/**
+ * @author Created by rui.zhang on 2018/4/9.
+ * @version ver1.0
+ * @email misterchangray@hotmail.com
+ * @description 根据数据库注释对实体类增加swagger2文档注解
+ */
+public class GeneratorBatchInsert  extends PluginAdapter {
+    public boolean validate(List<String> list) {
+        return false;
+    }
+}
---
+++ src/main/java/cn/zr/mybatis/generator/plugins/GeneratorSwagger2Doc.java
@@ -0,0 +1,40 @@
+package cn.zr.mybatis.generator.plugins;
+
+import org.mybatis.generator.api.*;
+import org.mybatis.generator.api.dom.java.Field;
+import org.mybatis.generator.api.dom.java.TopLevelClass;
+import org.mybatis.generator.config.TableConfiguration;
+
+import java.util.List;
+
+
+/**
+ * @author Created by rui.zhang on 2018/4/9.
+ * @version ver1.0
+ * @email misterchangray@hotmail.com
+ * @description 根据数据库注释对实体类增加swagger2文档注解
+ */
+public class GeneratorSwagger2Doc extends PluginAdapter {
+    public boolean validate(List<String> list) {
+        return true;
+    }
+
+    @Override
+    public boolean modelFieldGenerated(Field field, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType) {
+        String classAnnotation = "@ApiModel(value=\"" + topLevelClass.getType()  + "\")";
+        if(!topLevelClass.getAnnotations().contains(classAnnotation)) {
+            topLevelClass.addAnnotation(classAnnotation);
+        }
+
+        String apiModelAnnotationPackage =  properties.getProperty("apiModelAnnotationPackage");
+        String apiModelPropertyAnnotationPackage = properties.getProperty("apiModelPropertyAnnotationPackage");
+        if(null == apiModelAnnotationPackage) apiModelAnnotationPackage = "io.swagger.annotations.ApiModel";
+        if(null == apiModelPropertyAnnotationPackage) apiModelPropertyAnnotationPackage = "io.swagger.annotations.ApiModelProperty";
+
+        topLevelClass.addImportedType(apiModelAnnotationPackage);
+        topLevelClass.addImportedType(apiModelPropertyAnnotationPackage);
+
+        field.addAnnotation("@ApiModelProperty(value=\"" + introspectedColumn.getJavaProperty() + introspectedColumn.getRemarks() + "\")");
+        return super.modelFieldGenerated(field, topLevelClass, introspectedColumn, introspectedTable, modelClassType);
+    }
+}
