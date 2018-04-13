e79f821e026fcefcf40c6c241109ef32f29b584e
rui.zhang <zhangrui@ejie365.com>
2018-04-13 19:42:07 +0800

    生成swagger文档注解

---
+++ src/main/java/mybatis/generator/plugins/ExtendEntitySetter.java
@@ -0,0 +1,38 @@
+package mybatis.generator.plugins;
+
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+import org.mybatis.generator.api.dom.java.*;
+import org.mybatis.generator.api.dom.xml.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+/**
+ * @author Created by rui.zhang on 2018/4/9.
+ * @version ver1.0
+ * @email misterchangray@hotmail.com
+ * @description 对entity的set函数进行扩展，在设置完毕后返回当前实例;方便链式调用
+ */
+public class ExtendEntitySetter extends PluginAdapter {
+    private static final Logger logger = LoggerFactory.getLogger(GeneratorBatchInsert.class);
+
+    public boolean validate(List<String> list) {
+        return true;
+    }
+
+
+    @Override
+    public boolean modelSetterMethodGenerated(Method method, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType) {
+        method.setReturnType(topLevelClass.getType());
+        method.addBodyLine("return this;");
+        logger.debug("Setter扩展完成");
+        return super.modelSetterMethodGenerated(method, topLevelClass, introspectedColumn, introspectedTable, modelClassType);
+    }
+
+
+
+
+}
